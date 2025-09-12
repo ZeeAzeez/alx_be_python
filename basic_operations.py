5b3cffe0ba6d7df43aa564ddf3dca7089338b8c2
ZeeAzeez <azeezahaazeez@gmail.com>
2025-09-12 20:30:18 +0100

    completed task 0

--- basic_operations.py
+++ basic_operations.py
@@ -1,1 +1,10 @@
-print("Hello ZeeWorld!")
+number1 = 10;
+number2 = 5;
+
+sum = number1 + number2
+sub = number1 - number2 
+mul = number1 * number2 
+
+print(f"Addition of {number1} and {number2} is {sum}");
+print(f"Subtraction of {number1} and {number2} is {sub}");
+print(f"Multiplication of {number1} and {number2} is {mul}");
